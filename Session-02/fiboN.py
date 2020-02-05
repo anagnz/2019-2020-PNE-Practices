fcac1992386c5c67e3958cbb13aed32235ae1316
anaglez <a.gonzalezara.2019@alumnos.urjc.es>
2020-02-05 17:20:50 +0100

    exercise 2 from session 02

---
+++ Session-02/fiboN.py
@@ -0,0 +1,19 @@
+def fibo(n):
+    index1 = 0
+    index2 = 1
+    sumn = 0
+    for number in range(0, n+1):
+        if number == 0:
+            sumn = 0
+        elif number == 1:
+            sumn = 1
+        else:
+            sumn = index1 + index2
+            index1 = index2
+            index2 = sumn
+    return sumn
+
+
+print("5th Fibonacci termn: ", fibo(5))
+print("10th Fibonacci term: ", fibo(10))
+print("15th Fibonacci term: ", fibo(15))
