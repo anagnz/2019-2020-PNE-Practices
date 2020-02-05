25f5992c1c97ef526a28ff224bdaf9c70f0f125a
anaglez <a.gonzalezara.2019@alumnos.urjc.es>
2020-02-05 17:30:40 +0100

    exercise 3 from session 02

---
+++ Session-02/fibo-sumN.py
@@ -0,0 +1,22 @@
+def fibosum(n):
+    index1 = 0
+    index2 = 1
+    count = 0
+    sumn = 0
+    for number in range(0, n+1):
+        if number == 0:
+            count = 0
+            sumn = 0
+        elif number == 1:
+            count = 1
+            sumn = 1
+        else:
+            count = index1 + index2
+            index1 = index2
+            index2 = count
+            sumn += count
+    return sumn
+
+
+print("Sum of the First 5 terms of the Fibonacci series: ", fibosum(5))
+print("Sum of the First 10 terms of the Fibonacci series: ", fibosum(10))
