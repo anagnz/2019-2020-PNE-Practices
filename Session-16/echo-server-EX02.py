ad78a207b7bfd5073cdab67d3d7b11fd5c939b19
anago <a.gonzalezara.2019@alumnos.urjc.es>
2020-03-24 18:17:41 +0100

    Session 16 Finished

---
+++ Session-16/echo-server-EX02.py
@@ -0,0 +1,102 @@
+import http.server
+import socketserver
+import termcolor
+from pathlib import Path
+
+# Define the Server's port
+PORT = 8080
+
+
+# -- This is for preventing the error: "Port already in use"
+socketserver.TCPServer.allow_reuse_address = True
+
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        termcolor.cprint(self.requestline, 'green')
+
+        req_line = self.requestline.split()[1]
+
+        init = req_line.split("?")[0]
+
+        if init == "/":
+            contents = Path('form-EX02.html').read_text()
+            self.send_response(200)
+        elif init == "/echo":
+            text = req_line.split("=")[1]
+            value = text.split("&")[0]
+            if "&" in text:
+                contents = f"""
+                <!DOCTYPE html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>RESULT</title>
+                  </head>
+                  <body>
+                    <h2>Echoing the received message</h2>
+                    <p>{value.upper()}</p>  
+                    <p><p>
+                    <a href="http://127.0.0.1:8080/">Main page</a>
+                    </form>
+                  </body>
+                </html>
+                """
+            else:
+                contents = f"""
+                <!DOCTYPE html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>RESULT</title>
+                  </head>
+                  <body>
+                    <h2>Echoing the received message</h2>
+                    <p>{value.lower()}</p>  
+                    <p><p>
+                    <a href="http://127.0.0.1:8080/">Main page</a>
+                    </form>
+                  </body>
+                </html>
+                """
+            self.send_response(200)
+        else:
+            contents = Path('error.html').read_text()
+            self.send_response(404)
+
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(str.encode(contents))
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
