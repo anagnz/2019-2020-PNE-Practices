052867fefafecee508cd8133760b0f277517a4c1
anago <a.gonzalezara.2019@alumnos.urjc.es>
2020-05-24 17:46:09 +0200

    advanced level almost finished

--- Final-Project/client.py
+++ Final-Project/client.py
@@ -1,5 +1,4 @@
 import http.client
-import termcolor
 import json
 
 
@@ -12,70 +11,71 @@ def menu():
     5.- /geneInfo
     6.- /geneCalc
     7.- /geneList
-    8.- Finish
+    8.- Finish the loop
     """)
 
 
-def client(client_request):
+def client(client_request, json_param):
     port = 8080
     server = 'localhost'
     print(f"\nConnecting to server: {server}:{port}\n")
     conn = http.client.HTTPConnection(server, port)
-    
+
     try:
-        conn.request("GET", client_request + "&json=1")
+        conn.request("GET", client_request + "&json=" + json_param)
     except ConnectionRefusedError:
         print("ERROR! Cannot connect to the Server")
         exit()
-    
+
     r1 = conn.getresponse()
     print(f"Response received!: {r1.status} {r1.reason}\n")
     data1 = r1.read().decode("utf-8")
     data = json.loads(data1)
     print(f"CONTENT: {data}")
 
+
 valid = True
 while valid:
     menu()
+    json_param = input("Choose a json parameter(must be 1 for it to work): ")
     options = int(input("Choose an option: "))
 
     if options == 1:
         limit = input("Type the number of species you want to be shown: ")
         url = "/listSpecies?limit=" + limit
-        client(url)
+        client(url, json_param)
 
     elif options == 2:
         specie = input("Choose any specie: ")
         url = "/karyotype?specie=" + specie
-        client(url)
+        client(url, json_param)
 
     elif options == 3:
         specie = input("Choose specie: ")
         chromosome = input("Choose chromosome: ")
         url = f"/chromosomeLength?specie={specie}&chromo={chromosome}"
-        client(url)
-
+        client(url, json_param)
     elif options == 4:
         gene = input("Choose a human gene: ")
         url = "/geneSeq?gene=" + gene
-        client(url)
+        client(url, json_param)
 
     elif options == 5:
         gene = input("Choose a human gene: ")
         url = "/geneInfo?gene=" + gene
-        client(url)
+        client(url, json_param)
 
     elif options == 6:
         gene = input("Choose a human gene: ")
         url = "/geneCalc?gene=" + gene
-        client(url)
+        client(url, json_param)
 
     elif options == 7:
         chromo = input("Choose a human chromosome: ")
         start = input("Choose the start point: ")
         end = input("Choose the end point: ")
         url = f"/geneList?chromo={chromo}&start={start}&end={end}"
-        client(url)
+        client(url, json_param)
 
     elif options == 8:
         valid = False
